11c7f5545fa13e544cb2d3c5da8dadc6e619dc81
Wayne Dahlberg <waynefj40@gmail.com>
2023-06-13 23:50:04 -0600

    initial commit

---
+++ ThatsSoMetal/ThatsSoMetal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThatsSoMetal
+//
+//  Created by Wayne Dahlberg on 6/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "person")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThatsSoMetal/ThatsSoMetal/ThatsSoMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThatsSoMetalApp.swift
+//  ThatsSoMetal
+//
+//  Created by Wayne Dahlberg on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ThatsSoMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
